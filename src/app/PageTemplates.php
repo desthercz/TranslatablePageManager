c3361ff8112da1075c14a8096e63a8b15f2732d4
desthercz <desther@seznam.cz>
2019-05-27 23:49:03 +0200

    Update PageTemplates.php

--- src/app/PageTemplates.php
+++ src/app/PageTemplates.php
@@ -6,11 +6,11 @@ trait PageTemplates
 {
     /*
     |--------------------------------------------------------------------------
-    | Page Templates for Backpack\PageManager
+    | Page Templates for Backpack\TranslatablePageManager
     |--------------------------------------------------------------------------
     |
     | Each page template has its own method, that define what fields should show up using the Backpack\CRUD API.
-    | Use snake_case for naming and PageManager will make sure it looks pretty in the create/update form
+    | Use snake_case for naming and TranslatablePageManager will make sure it looks pretty in the create/update form
     | template dropdown.
     |
     | Any fields defined here will show up after the standard page fields:
